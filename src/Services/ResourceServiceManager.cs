e9f4b4f1bbeb1b92cc65a961c0d03d997c47d197
KGergo02 <kassa.gergo2002@gmail.com>
2023-12-25 21:41:46 +0100

    Added ResourceServiceManager for easier access to the icon of the application

---
+++ src/Services/ResourceServiceManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teszt__.src.Services
+{
+    public static class ResourceServiceManager
+    {
+        public static readonly string Icon = "../../src/Resources/favicon.ico";
+    }
+}
