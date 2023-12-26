6c7e167c65ffec73123c8b8d9cf925322a1083b3
KGergo02 <kassa.gergo2002@gmail.com>
2023-12-26 19:23:00 +0100

    Created profile view

---
+++ src/ViewModels/User_ViewModels/UserProfileViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows.Navigation;
+using Teszt__.src.Commands.User_Commands;
+using Teszt__.src.Models;
+using Teszt__.src.Views.User_Views;
+
+namespace Teszt__.src.ViewModels.User_ViewModels
+{
+    public class UserProfileViewModel : ViewModelBase
+    {
+        public UserProfileViewModel(DatabaseContext.User user, UserProfileView window ,NavigationWindow navigationWindow)
+        {
+            EditUserInfoCommand = new EditUserInfoCommand(user, window);
+
+            DeleteUserCommand = new DeleteUserCommand(user, window, navigationWindow);
+        }
+
+        public ICommand EditUserInfoCommand { get; }
+        public ICommand DeleteUserCommand { get; }
+    }
+}
