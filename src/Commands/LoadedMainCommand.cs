8a934443f2fadc3982e55058f95321e5ec89fee6
KGergo02 <kassa.gergo2002@gmail.com>
2023-12-26 23:17:26 +0100

    Removed unneccessary usings of libraries

--- src/Commands/LoadedMainCommand.cs
+++ src/Commands/LoadedMainCommand.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Teszt__.src.DAL;
-using static Teszt__.src.Models.DatabaseContext;
+﻿using Teszt__.src.DAL;
 
 namespace Teszt__.src.Commands
 {
