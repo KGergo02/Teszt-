a055040447a1c6e6c11a1207946f2b561fd7923e
KGergo02 <kassa.gergo2002@gmail.com>
2024-01-03 17:36:37 +0100

    Added ControlEventService for a course card label event which collapses the test labels below them.

---
+++ src/Services/ControlEventService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Teszt__.src.Services
+{
+    public class ControlEventService
+    {
+        public static void OnCourseCardLabelClick(object sender, MouseButtonEventArgs args)
+        {
+            Label courseLabel = (Label)sender;
+
+            foreach (Label item in (List<Label>)courseLabel.Tag)
+            {
+                if(item.IsVisible)
+                {
+                    item.Visibility = System.Windows.Visibility.Collapsed;
+                }
+                else
+                {
+                    item.Visibility = System.Windows.Visibility.Visible;
+                }
+            }
+        }
+    }
+}
