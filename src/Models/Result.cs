e4edc3a46730b5fd585faca350e650e394be9553
KGergo02 <kassa.gergo2002@gmail.com>
2024-02-15 20:52:50 +0100

    Made a new model for storing student's results in the database

---
+++ src/Models/Result.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Teszt__.src.Services;
+
+namespace Teszt__.src.Models
+{
+    public partial class DatabaseContext
+    {
+        public class Result
+        {
+            public int Id { get; set; }
+            
+            public string Username { get; set; }
+
+            public int Value { get; set; }
+
+            public int TestId { get; set; }
+
+            public string Date { get; set; }
+
+            public Result()
+            {
+            }
+
+            public Result(string username, int value, int testId, string dateTime)
+            {
+                Username = username;
+                Value = value;
+                TestId = testId;
+                Date = dateTime;
+            }
+        }
+    }
+}
