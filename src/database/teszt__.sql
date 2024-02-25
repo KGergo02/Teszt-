23a375563295351e0913203d5a4734ec2e77348f
KGergo02 <kassa.gergo2002@gmail.com>
2024-02-25 18:47:42 +0100

    Database upload

--- src/database/teszt__.sql
+++ src/database/teszt__.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Gép: localhost
--- Létrehozás ideje: 2024. Feb 15. 18:28
+-- Létrehozás ideje: 2024. Feb 25. 18:46
 -- Kiszolgáló verziója: 10.4.25-MariaDB
 -- PHP verzió: 8.1.10
 
@@ -41,38 +41,29 @@ CREATE TABLE `answers` (
 --
 
 INSERT INTO `answers` (`id`, `value`, `correct`, `questionId`) VALUES
-(12, 'Hétfő', 1, 26),
-(13, 'A', 1, 27),
-(14, 'B', 0, 27),
-(15, 'C', 0, 27),
-(16, 'D', 1, 27),
-(17, '2', 1, 28),
-(18, '1', 0, 28),
-(19, '0', 0, 28),
-(20, 'A', 0, 29),
-(21, 'B', 1, 29),
-(22, 'C', 0, 29),
-(23, 'A', 1, 30),
-(24, 'B', 0, 30),
-(25, 'A', 0, 31),
-(26, 'B', 0, 31),
-(27, 'C', 1, 31),
-(28, 'A', 0, 32),
-(29, 'Teszt', 1, 32),
-(30, 'B', 0, 32),
-(31, 'ASFD', 0, 33),
-(32, 'ASD', 1, 33),
-(33, 'ABS', 0, 33),
-(34, 'ASDF', 1, 34),
-(35, 'ASED', 0, 34),
-(36, 'ASB', 0, 34),
-(37, 'PPP', 1, 35),
-(38, 'ppp', 0, 35),
-(39, 'aaa', 0, 35),
-(40, 'for', 1, 36),
-(41, 'D', 1, 37),
-(42, 'A', 0, 37),
-(43, 'B', 0, 37);
+(73, 'Szombat', 1, 54),
+(74, 'Igen', 1, 55),
+(75, '3', 0, 56),
+(76, 'Free', 1, 56),
+(77, 'Three', 0, 56),
+(94, 'A', 1, 60),
+(95, 'B', 1, 60),
+(96, 'C', 1, 60),
+(97, 'D', 0, 60),
+(98, 'Csütörtök', 1, 61),
+(99, 'TeSzt', 1, 62),
+(100, 'Teszt', 1, 62),
+(101, 'Asd', 0, 62),
+(102, 'AsD', 0, 62),
+(103, 'ASD', 1, 63),
+(104, '1', 0, 63),
+(105, '2', 0, 63),
+(106, '231', 1, 64),
+(107, '321', 0, 64),
+(108, '123', 0, 64),
+(109, 'A', 1, 65),
+(110, 'B', 0, 65),
+(111, 'C', 0, 65);
 
 -- --------------------------------------------------------
 
@@ -94,7 +85,7 @@ INSERT INTO `courses` (`id`, `name`, `user_limit`) VALUES
 (1, 'Kurzus1', 10),
 (2, 'Programozás I.', 200),
 (3, 'Adatbázisok', 100),
-(4, 'Filler', 10);
+(4, 'Filler', 1);
 
 -- --------------------------------------------------------
 
@@ -115,18 +106,15 @@ CREATE TABLE `questions` (
 --
 
 INSERT INTO `questions` (`id`, `name`, `questionType`, `value`, `testId`) VALUES
-(26, 'Milyen nap van ma?', 'Szöveges válasz', 5, 9),
-(27, 'A és D', 'Jelölődoboz', 2, 9),
-(28, '1+1?', 'Rádiógomb', 5, 9),
-(29, 'B', 'Jelölődoboz', 10, 9),
-(30, 'A', 'Jelölődoboz', 10, 9),
-(31, 'C', 'Jelölődoboz', 10, 9),
-(32, 'Teszt', 'Jelölődoboz', 20, 9),
-(33, 'ASD', 'Jelölődoboz', 30, 9),
-(34, 'ASDF', 'Jelölődoboz', 5, 9),
-(35, 'PPP', 'Rádiógomb', 10, 9),
-(36, 'Code', 'Szöveges válasz', 30, 9),
-(37, 'D', 'Jelölődoboz', 5, 9);
+(54, 'Milyen nap van ma?', 'Rövid választ igénylő feladat', 10, 24),
+(55, 'El tudtad indítani?', 'Rövid választ igénylő feladat', 10, 25),
+(56, 'Free', 'Rádiógomb', 10, 26),
+(60, 'ABC első három betűje', 'Jelölődoboz', 10, 24),
+(61, 'Milyen nap van ma?', 'Rövid választ igénylő feladat', 10, 28),
+(62, 'Teszt', 'Jelölődoboz', 10, 28),
+(63, 'ASD', 'Rádiógomb', 10, 29),
+(64, '231', 'Rádiógomb', 20, 29),
+(65, 'A', 'Rádiógomb', 10, 24);
 
 -- --------------------------------------------------------
 
@@ -147,9 +135,10 @@ CREATE TABLE `results` (
 --
 
 INSERT INTO `results` (`id`, `username`, `value`, `testid`, `date`) VALUES
-(9, 'ASD', 10, 9, '2024. 02. 15. 18:21:01'),
-(10, 'Próba', 35, 9, '2024. 02. 15. 18:22:11'),
-(11, 'Próba', 0, 12, '2024. 02. 15. 18:28:26');
+(96, 'ASD', 30, 24, '2024. 02. 23. 16:08:18'),
+(97, 'ASD', 20, 28, '2024. 02. 23. 16:08:28'),
+(98, 'ASD', 0, 28, '2024. 02. 23. 16:08:33'),
+(102, 'ASD', 30, 29, '2024. 02. 23. 18:38:09');
 
 -- --------------------------------------------------------
 
@@ -161,8 +150,10 @@ CREATE TABLE `tests` (
   `id` int(11) NOT NULL,
   `name` varchar(100) COLLATE utf8_hungarian_ci NOT NULL,
   `submit_limit` int(11) NOT NULL,
-  `date` varchar(100) COLLATE utf8_hungarian_ci NOT NULL,
+  `best_submitted_result_counts` tinyint(1) NOT NULL,
+  `startDate` varchar(100) COLLATE utf8_hungarian_ci NOT NULL,
   `startTime` varchar(100) COLLATE utf8_hungarian_ci NOT NULL,
+  `endDate` varchar(100) COLLATE utf8_hungarian_ci NOT NULL,
   `endTime` varchar(100) COLLATE utf8_hungarian_ci NOT NULL,
   `CourseId` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
@@ -171,13 +162,12 @@ CREATE TABLE `tests` (
 -- A tábla adatainak kiíratása `tests`
 --
 
-INSERT INTO `tests` (`id`, `name`, `submit_limit`, `date`, `startTime`, `endTime`, `CourseId`) VALUES
-(1, 'League Of Legends', 1, '2023. 12. 16.', '10:00', '12:00', 1),
-(2, 'Teszt1', 1, '2024. 01. 04.', '12:00', '13:00', NULL),
-(3, 'Teszt2', 1, '2024. 01. 05.', '12:00', '12:05', NULL),
-(5, 'Correct', 1, '2024. 01. 03.', '16:00', '16:02', 1),
-(9, 'Mindig indítható', 100, '2024. 02. 15.', '00:00', '24:00', 3),
-(12, 'Nem indítható', 1, '2024. 02. 15.', '10:00', '20:00', 4);
+INSERT INTO `tests` (`id`, `name`, `submit_limit`, `best_submitted_result_counts`, `startDate`, `startTime`, `endDate`, `endTime`, `CourseId`) VALUES
+(24, 'Mindig indítható', 100, 0, '2024. 02. 17.', '00:00', '9999. 01. 01.', '23:59', 3),
+(25, 'Határidő lejárt', 100, 1, '2024. 02. 17.', '10:00', '2024. 02. 18.', '15:13', 2),
+(26, 'Nem indítható', 1, 0, '2024. 02. 16.', '00:00', '2024. 02. 16.', '23:59', 2),
+(28, 'Mai nap', 2, 1, '2024. 02. 22.', '18:00', '2024. 02. 23.', '18:13', 3),
+(29, 'ASD', 2, 1, '2024. 02. 23.', '00:00', '2024. 02. 23.', '23:59', 2);
 
 -- --------------------------------------------------------
 
@@ -199,8 +189,7 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `name`, `password`, `email`, `admin`) VALUES
 (1, 'KGergo02', 'jJYWPRZl0WA=', 'kassa.gergo2002@gmail.com', 1),
-(2, 'ASD', '/rA2D/Mil+M=', 'asd@asd.com', 1),
-(5, 'Próba', '/rA2D/Mil+M=', 'teszt@test.com', 0);
+(2, 'ASD', '/rA2D/Mil+M=', 'asd@asd.com', 1);
 
 -- --------------------------------------------------------
 
@@ -219,11 +208,8 @@ CREATE TABLE `user_courses` (
 --
 
 INSERT INTO `user_courses` (`id`, `user_name`, `course_name`) VALUES
-(1, 'ASD', 'Kurzus1'),
 (2, 'ASD', 'Adatbázisok'),
-(3, 'ASD', 'Filler'),
-(6, 'Próba', 'Adatbázisok'),
-(7, 'Próba', 'Filler');
+(16, 'ASD', 'Programozás I.');
 
 --
 -- Indexek a kiírt táblákhoz
@@ -288,31 +274,31 @@ ALTER TABLE `user_courses`
 -- AUTO_INCREMENT a táblához `answers`
 --
 ALTER TABLE `answers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=112;
 
 --
 -- AUTO_INCREMENT a táblához `courses`
 --
 ALTER TABLE `courses`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT a táblához `questions`
 --
 ALTER TABLE `questions`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=66;
 
 --
 -- AUTO_INCREMENT a táblához `results`
 --
 ALTER TABLE `results`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=103;
 
 --
 -- AUTO_INCREMENT a táblához `tests`
 --
 ALTER TABLE `tests`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 
 --
 -- AUTO_INCREMENT a táblához `users`
@@ -324,7 +310,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT a táblához `user_courses`
 --
 ALTER TABLE `user_courses`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- Megkötések a kiírt táblákhoz
@@ -346,8 +332,8 @@ ALTER TABLE `questions`
 -- Megkötések a táblához `results`
 --
 ALTER TABLE `results`
-  ADD CONSTRAINT `TesztId` FOREIGN KEY (`testid`) REFERENCES `tests` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `fk_Username` FOREIGN KEY (`username`) REFERENCES `users` (`name`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `TesztId` FOREIGN KEY (`testid`) REFERENCES `tests` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_Username` FOREIGN KEY (`username`) REFERENCES `users` (`name`) ON DELETE NO ACTION ON UPDATE CASCADE;
 
 --
 -- Megkötések a táblához `tests`
