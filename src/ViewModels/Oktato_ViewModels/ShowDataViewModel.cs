2b9ff1fbdc9ec74ad8ce3b460fd314955cb43a0d
KGergo02 <kassa.gergo2002@gmail.com>
2024-02-25 14:13:18 +0100

    Plugged in the buttons for ShowDataViewModel

--- src/ViewModels/Oktato_ViewModels/ShowDataViewModel.cs
+++ src/ViewModels/Oktato_ViewModels/ShowDataViewModel.cs
@@ -1,4 +1,8 @@
 ﻿using System.Windows.Controls;
+using System.Windows.Input;
+using Teszt__.src.Commands;
+using Teszt__.src.Commands.Oktato_Commands;
+using Teszt__.src.Commands.User_Commands;
 using Teszt__.src.Services;
 
 namespace Teszt__.src.ViewModels.Oktato_ViewModels
@@ -7,7 +11,17 @@ namespace Teszt__.src.ViewModels.Oktato_ViewModels
     {
         public ShowDataViewModel(string mode, StackPanel mainStackPanel, Button sendButton)
         {
+            NavigateToOktatoViewCommand = new NavigateToOktatoViewCommand();
+
+            ShowUserProfileCommand = new ShowUserProfileCommand();
+
+            LogOutCommand = new LogOutCommand();
+
             DataGridService.CreateDataGrid(mode, ref mainStackPanel, ref sendButton);
         }
+
+        public ICommand NavigateToOktatoViewCommand { get; }
+        public ICommand ShowUserProfileCommand { get; }
+        public ICommand LogOutCommand { get; }
     }
 }
